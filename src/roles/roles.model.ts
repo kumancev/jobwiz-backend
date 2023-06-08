3b90bf9ad4e07c2eccade63dcfaf54d787b89c5f
Alex Kumancev <kumancev1@gmail.com>
2023-06-08 16:45:52 +0300

    feat: add Roles

---
+++ src/roles/roles.model.ts
@@ -0,0 +1,32 @@
+import { ApiProperty } from '@nestjs/swagger'
+import { Column, DataType, Model, Table } from 'sequelize-typescript'
+
+interface RoleCreationAttrs {
+  name: string
+  description: string
+}
+
+@Table({ tableName: 'roles' })
+export class Role extends Model<Role, RoleCreationAttrs> {
+  @ApiProperty({ example: '1', description: 'Unique identifier' })
+  @Column({
+    type: DataType.INTEGER,
+    unique: true,
+    autoIncrement: true,
+    primaryKey: true,
+  })
+  id: number
+  @ApiProperty({ example: 'ADMIN', description: 'User role name' })
+  @Column({
+    type: DataType.STRING,
+    unique: true,
+    allowNull: false,
+  })
+  name: string
+  @ApiProperty({ example: 'Admin', description: 'Role description' })
+  @Column({
+    type: DataType.STRING,
+    allowNull: false,
+  })
+  description: string
+}
--- src/users/users.model.ts
+++ src/users/users.model.ts
@@ -8,7 +8,7 @@ interface UserCreationAttrs {
 
 @Table({ tableName: 'users' })
 export class User extends Model<User, UserCreationAttrs> {
-  @ApiProperty({ example: '1', description: 'User ID' })
+  @ApiProperty({ example: '1', description: 'Unique identifier' })
   @Column({
     type: DataType.INTEGER,
     unique: true,
