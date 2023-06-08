ea7ecf17267be6041619224870554312ff297437
Alex Kumancev <kumancev1@gmail.com>
2023-06-08 16:59:09 +0300

    feat: add UserRoles linked table

--- src/roles/roles.model.ts
+++ src/roles/roles.model.ts
@@ -1,5 +1,13 @@
 import { ApiProperty } from '@nestjs/swagger'
-import { Column, DataType, Model, Table } from 'sequelize-typescript'
+import {
+  BelongsToMany,
+  Column,
+  DataType,
+  Model,
+  Table,
+} from 'sequelize-typescript'
+import { User } from '../users/users.model'
+import { UserRoles } from './user-roles.model'
 
 interface RoleCreationAttrs {
   name: string
@@ -29,4 +37,7 @@ export class Role extends Model<Role, RoleCreationAttrs> {
     allowNull: false,
   })
   description: string
+
+  @BelongsToMany(() => User, () => UserRoles)
+  users: User[]
 }
